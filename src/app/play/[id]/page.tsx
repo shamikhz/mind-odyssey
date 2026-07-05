d91e88d856634a6c966d2ea004737ad6132f8e5f
shamikhz <shamikhzafar0@gmail.com>
2026-07-05 21:25:51 +0530

    Fix Cloudflare edge runtime config by using a server component layout

--- src/app/play/[id]/page.tsx
+++ src/app/play/[id]/page.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-export const runtime = 'edge';
-
 import React, { useState, useEffect, useCallback, lazy, Suspense } from 'react';
 import { useParams, useRouter } from 'next/navigation';
 import Link from 'next/link';
